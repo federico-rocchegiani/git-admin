f9f5b94c924a9c2f4f204ab66e46325733b07cc1
Federico Rocchegiani <federico.rocchegiani@pixartprinting.com>
2016-02-03 22:14:37 +0100

    Added config manage

---
+++ app/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = array();
+
+# put your user home (generally /home/username) this is where your global .git
+# is stored
+$config['home'] = '';
+
+# put your additional path to be used to find executables, so if you have any
+# particular stuff in your git-hooks, they will be executed
+$config['path'] = '';
--- core/Controller.php
+++ core/Controller.php
@@ -2,12 +2,21 @@
 
 namespace core;
 
+use core\Utility as U;
+
 class Controller
 {
 
+    protected $config = array();
+    
     public function __construct()
     {
-        
+        $configFile = U::$APP.'config.php';
+        if(file_exists($configFile)){
+            $config = array();
+            require_once $configFile;
+            $this->config = $config;
+        }
     }
 
 }
